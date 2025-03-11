452e73ccb1bce19c9659f7e10ba792d7d1ecaea2
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2025-03-11 23:47:15 +0500

    home page creating button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
+import LoginButton from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <>
-      <h2 className="font-bold text-4xl">Hello Auth!</h2>;<Button>Click</Button>
-    </>
+    <main className="flex h-full flex-col items-center justify-center bg-blue-500">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          🔏 Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }

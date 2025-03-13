2674d4606b8702ee6d6cb76beebbb29693158907
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2025-03-13 13:22:54 +0500

    make register components and action

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,3 +4,8 @@ export const LoginSchema = z.object({
   email: z.string().email({ message: "email is required" }),
   password: z.string().min(3, { message: "password is required" }),
 });
+export const RegisterSchema = z.object({
+  name: z.string().min(3, { message: "name is required" }),
+  email: z.string().email({ message: "email is required" }),
+  password: z.string().min(3, { message: "password is required" }),
+});

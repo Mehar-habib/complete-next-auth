9e8cf3c34c29a39dc1c4ab76097b34f2def511c2
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2025-03-14 12:34:21 +0500

    callback

--- auth.ts
+++ auth.ts
@@ -4,6 +4,17 @@ import { db } from "./lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,

ae0929232e42877585a055c8461009f223d61abe
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2025-03-14 11:47:02 +0500

    (continue)

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,28 @@
-import GitHub from "next-auth/providers/github";
 import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "./schemas";
+import bcrypt from "bcryptjs";
+import { getUserByEmail } from "./data/user";
 
-export default { providers: [GitHub] } satisfies NextAuthConfig;
+export default {
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        // If validation fails, return null
+        if (!validatedFields.success) return null;
+
+        const { email, password } = validatedFields.data; // Access data only if validation is successful
+
+        const user = await getUserByEmail(email);
+        if (!user || !user.password) return null;
+
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+        if (passwordsMatch) return user;
+
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;

3a68a7a989e0b6544a5952abdbf1f0248ee859de
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2025-03-19 22:22:30 +0500

    complete reset UI and functionality

---
+++ components/auth/new-password-form.tsx
@@ -0,0 +1,96 @@
+"use client";
+import * as z from "zod";
+import CardWrapper from "./card-wrapper";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import FormError from "../form-error";
+import { newPassword } from "@/actions/new-password";
+import { useEffect, useState, useTransition } from "react";
+import FormSuccess from "../form-success";
+import { NewPasswordSchema } from "@/schemas";
+import { useSearchParams } from "next/navigation";
+
+export default function NewPasswordForm() {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+
+  const [isPending, startTransition] = useTransition();
+  const form = useForm<z.infer<typeof NewPasswordSchema>>({
+    resolver: zodResolver(NewPasswordSchema),
+    defaultValues: {
+      password: "",
+    },
+  });
+  const onSubmit = (values: z.infer<typeof NewPasswordSchema>) => {
+    setError("");
+    setSuccess("");
+    startTransition(() => {
+      newPassword(values, token).then((data) => {
+        setError(data?.error);
+        setSuccess(data?.success);
+      });
+    });
+  };
+  useEffect(() => {
+    if (error || success) {
+      const timer = setTimeout(() => {
+        setError("");
+        setSuccess("");
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [error, success]);
+
+  return (
+    <CardWrapper
+      headerLabel="Enter a new password?"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            {/* email */}
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isPending}
+                      {...field}
+                      placeholder="*******"
+                      type="password"
+                      className="py-5 tracking-wider"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button type="submit" className="w-full " disabled={isPending}>
+            Reset Password
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+}
--- schemas/index.ts
+++ schemas/index.ts
@@ -13,3 +13,7 @@ export const RegisterSchema = z.object({
 export const ResetSchema = z.object({
   email: z.string().email({ message: "email is required" }),
 });
+
+export const NewPasswordSchema = z.object({
+  password: z.string().min(3, { message: "Minimum of 3 characters required" }),
+});

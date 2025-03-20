1ebf62fdbdee1dbba7b4d16fd3ba4f7af38962e0
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2025-03-21 02:27:42 +0500

    continue

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,16 +1,15 @@
 "use client";
 import { logout } from "@/actions/logout";
 import { Button } from "@/components/ui/button";
-import { useSession } from "next-auth/react";
+// import { useCurrentUser } from "@/hooks/use-current-user";
 
 const SettingsPage = () => {
-  const session = useSession();
+  // const user = useCurrentUser();
   const onClick = () => {
     logout();
   };
   return (
-    <div>
-      {JSON.stringify(session.data?.user)}
+    <div className="bg-white p-10 rounded-xl">
       <Button type="submit" onClick={onClick}>
         Sign out
       </Button>

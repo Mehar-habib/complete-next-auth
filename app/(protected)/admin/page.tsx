2fcb1b78a12d1f98c6208e936257b3c3e6408c06
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2025-03-21 17:19:41 +0500

    complete admin page

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { admin } from "@/actions/admin";
 import { RoleGate } from "@/components/auth/role-gate";
 import FormSuccess from "@/components/form-success";
 import { Button } from "@/components/ui/button";
@@ -7,6 +8,16 @@ import { UserRole } from "@prisma/client";
 import { toast } from "sonner";
 
 const AdminPage = () => {
+  const onServerActionClick = async () => {
+    admin().then((data) => {
+      if (data.error) {
+        toast.error(data.error);
+      }
+      if (data.success) {
+        toast.success(data.success);
+      }
+    });
+  };
   const onApiRouteClick = () => {
     fetch("/api/admin").then((response) => {
       if (response.ok) {
@@ -33,7 +44,7 @@ const AdminPage = () => {
 
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
           <p className="text-sm font-medium">Admin-only Server Action</p>
-          <Button>Click to test</Button>
+          <Button onClick={onServerActionClick}>Click to test</Button>
         </div>
       </CardContent>
     </Card>
